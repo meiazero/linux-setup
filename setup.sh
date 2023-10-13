189ae951d5aa93e757ccc3d5f1cb3458055503ed
Emanuel Avila <avilaemanueel@gmail.com>
2023-10-13 19:09:53 -0300

    feat: initial setup

---
+++ setup.sh
@@ -0,0 +1,75 @@
+# TODO: Fazer a instalação dos plugins zsh-autosuggestion e zsh-syntaxhighlight, 
+# TODO: pnpm
+# TODO: oh-my-zsh e powerlevel10k, 
+# TODO: asdf com os plugins de java(openjdk17 e openjdk21), nodejs(18.18.1 e 20.8.0), ruby latest, python(3.11, 3.10, 3.9), rust latest, lua latest
+# TODO: instalar vscode
+# TODO: instalar dbeaver
+# TODO: instalar docker
+# TODO: instalar insonminia
+# TODO: instalar pycharm, intelij, webstorm
+# TODO: instalar 
+
+#!/usr/bin/bash
+
+user_id="$(id -u)"
+if [ "$user_id" -ne 0 ]; then
+  echo "
+# The script:
+#
+# - Requires 'root' or 'sudo' privileges to run.
+"
+  exit 1
+fi
+
+_NALA="nala"
+sh_c='sh -c'
+
+pkgs=("curl" "wget" "zsh" "git" "vim" "exa" "fonts-jetbrains-mono"
+      "fonts-inconsolata" "build-essential" "nmap" "hydra" "bat" "make" "tor")
+
+nala_exists(){
+  local bin_paths=("/usr/local/bin" "/usr/bin" "/bin")
+  
+  for path in "${bin_paths[@]}"; do
+    if [[ -x "$path/nala" ]]; then
+      nala_exists=1
+      break
+    fi
+  done
+
+  if [ "$nala_exists" -eq 1 ]; then
+    echo "-> Nala exists in the system."   
+  else
+    echo "
+# 
+# Instaling nala in $path
+#
+"
+    install_nala
+  fi
+}
+
+install_nala() {
+  $sh_c "DEBIAN_FRONTEND=noninteractive apt-get install -y -qq $_NALA >/dev/null"
+  echo "
+# 
+# Nala installed successfully
+#
+"
+}
+
+do_install() {
+  nala_exists
+  for pkg in "${pkgs[@]}"; do
+    $_NALA install -y $pkg  
+  done
+  
+  echo "
+# 
+# All packages has installed
+#
+"
+}
+
+do_install
+
